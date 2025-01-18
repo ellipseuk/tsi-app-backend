40541ad2dec4029a6064ad189a981a0b635abd01
ellipseuk <daniilgustenko@gmail.com>
2025-01-18 22:28:48 +0200

    refactor: remove auth and staff controllers

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ import express, { json } from 'express';
 
 import newsRoutes from './routes/news.js';
 import eventsRoutes from './routes/events.js';
-import staffRoutes from './routes/staff.js';
-import authRoutes from './routes/auth.js';
 
 const app = express();
 
@@ -14,8 +12,6 @@ app.use(json());
 
 app.use('/api', newsRoutes);
 app.use('/api', eventsRoutes);
-app.use('/api', staffRoutes);
-app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
--- routes/staff.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getAllStaff, getStaffByName } from '../controllers/staffController.js';
-
-const router = express.Router();
-
-router.get('/staff', getAllStaff);
-router.get('/staff/:name', getStaffByName);
-
-export default router;
